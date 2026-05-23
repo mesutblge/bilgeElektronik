11a9620d671ed9c1893bd7bc4093203827deb238
mesut.bilge <mesut.bilge@data-boss.com.tr>
2026-05-23 18:49:06 +0300

    Google Analytics eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -64,7 +65,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={inter.className}>
         {children}
-</body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-CSERRSFD19" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CSERRSFD19');
+        `}</Script>
+      </body>
     </html>
   )
 }
